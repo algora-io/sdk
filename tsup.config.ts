3b155354288831d1bdb6451b81d28b779485ca48
kaf-lamed-beyt <belac335@gmail.com>
2023-09-30 14:48:36 +0100

    chore: add commonJS support

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   minify: !isDev,
   metafile: !isDev,
   sourcemap: true,
